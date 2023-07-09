def06b5995f3215aabb3e752d43a5d8898b13166
Ronit kaushik <94043046+thatboredbeast@users.noreply.github.com>
2023-07-09 17:09:10 +0530

    app.js

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ const popularMovies = async () => {
         const res = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=2d3fe3e2f63c08b2a0f90dd1dacd62c9");
         const data = await res.json();
         let n = Math.floor(Math.random() * 20);
-        console.log(data.results);
-        console.log(n);
-
         const heading = document.getElementById('heading');
         const moviesDescription = document.getElementById('movies');
         heading.append(data.results[n].title);
